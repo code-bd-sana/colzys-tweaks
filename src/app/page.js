d6acc4435a18ef99794397b45964f24c58da0ffe
rakib3719 <rakib3719>
2026-02-05 10:43:45 +0600

    feature section design done

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Banner from "@/components/homepage/Banner";
+import Feature from "@/components/homepage/feature/Feature";
 
 export default function Home() {
   return (
     <div className=''>
       <Banner />
+      <Feature />
     </div>
   );
 }
